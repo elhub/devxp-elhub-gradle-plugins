ceb2b5599e59531a923f0bdfe5272fc016fa570d
Michael Akinde <michael.akinde@statnett.no>
2024-06-13 14:57:23 +0200

    Update TeamCity configuration to v0.105.1

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,79 +1,20 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
-import jetbrains.buildServer.configs.kotlin.v2019_2.project
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2019_2.version
-import no.elhub.devxp.build.configuration.AutoRelease
-import no.elhub.devxp.build.configuration.CodeReview
-import no.elhub.devxp.build.configuration.ProjectType
-import no.elhub.devxp.build.configuration.SonarScan
-import no.elhub.devxp.build.configuration.UnitTest
+import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubProject
+import no.elhub.devxp.build.configuration.pipeline.constants.Group.DEVXP
+import no.elhub.devxp.build.configuration.pipeline.constants.ArtifactoryRepository
+import no.elhub.devxp.build.configuration.pipeline.jobs.gradleAutoRelease
+import no.elhub.devxp.build.configuration.pipeline.jobs.gradleVerify
 
-version = "2022.10"
+elhubProject(DEVXP, "devxp-elhub-gradle-plugins") {
+    val artifactoryRepository = ArtifactoryRepository.ELHUB_PLUGINS_RELEASE_LOCAL
 
-project {
-    val projectName = "devxp-elhub-gradle-plugins"
-    val projectId = "no.elhub.devxp:$projectName"
-    val projectType = ProjectType.GRADLE
-    val artifactoryRepository = "elhub-plugins-release-local"
-
-    params {
-        param("teamcity.ui.settings.readOnly", "true")
-    }
-
-    val unitTest = UnitTest(
-        UnitTest.Config(
-            vcsRoot = DslContext.settingsRoot,
-            type = projectType,
-            generateAllureReport = false,
-        )
-    )
-
-    val sonarScanConfig = SonarScan.Config(
-        vcsRoot = DslContext.settingsRoot,
-        type = projectType,
-        sonarId = projectId,
-        sonarProjectSources = "src/main",
-        additionalParams = listOf(
-            "-Dsonar.dependencyCheck.jsonReportPath=build/reports/dependency-check-report.json",
-            "-Dsonar.dependencyCheck.htmlReportPath=build/reports/dependency-check-report.html",
-        )
-    )
-
-    val sonarScan = SonarScan(sonarScanConfig) {
-        dependencies {
-            snapshot(unitTest) { }
-        }
-    }
-
-    val autoRelease = AutoRelease(
-        AutoRelease.Config(
-            vcsRoot = DslContext.settingsRoot,
-            type = projectType,
-            repository = artifactoryRepository
-        )
-    ) {
-        dependencies {
-            snapshot(sonarScan) { }
-        }
-        triggers {
-            vcs {
-                branchFilter = "+:<default>"
-                quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_DEFAULT
+    pipeline {
+        sequential {
+            val artifacts = gradleVerify {
+                analyzeDependencies = false
+            }
+            gradleAutoRelease(artifacts = listOf(artifacts)) {
+                repository = artifactoryRepository
             }
         }
     }
-
-    listOf(unitTest, sonarScan, autoRelease).forEach { buildType(it) }
-
-    buildType(
-        CodeReview(
-            CodeReview.Config(
-                vcsRoot = DslContext.settingsRoot,
-                type = projectType,
-                sonarScanConfig = sonarScanConfig,
-            )
-        )
-    )
-
 }
