5c5366672ff2d84c3a0deba92e85a5d8f2509681
Johannes Schaanning <johannes.schaanning@statnett.no>
2024-10-04 14:36:50 +0200

    chore: update build configuration version (#6)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,15 +5,13 @@ import no.elhub.devxp.build.configuration.pipeline.jobs.gradleAutoRelease
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleVerify
 
 elhubProject(DEVXP, "devxp-elhub-gradle-plugins") {
-    val artifactoryRepository = ArtifactoryRepository.ELHUB_PLUGINS_RELEASE_LOCAL
-
     pipeline {
         sequential {
             val artifacts = gradleVerify {
                 analyzeDependencies = false
             }
             gradleAutoRelease(artifacts = listOf(artifacts)) {
-                repository = artifactoryRepository
+                repository = ArtifactoryRepository.ELHUB_PLUGINS_RELEASE_LOCAL
             }
         }
     }
