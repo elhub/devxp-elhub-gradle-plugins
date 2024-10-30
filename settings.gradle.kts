baaf7c715a0c706c97459a7c95ebe8ef31c532f6
Michael Akinde <michael.akinde@statnett.no>
2024-10-30 09:43:31 +0100

    Ensure project builds again

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "elhub-gradle-plugins"
 
 pluginManagement {
     repositories {
+        maven(url = "https://jfrog.elhub.cloud/artifactory/elhub-mvn/")
         maven(url = "https://jfrog.elhub.cloud/artifactory/elhub-plugins")
     }
 }
