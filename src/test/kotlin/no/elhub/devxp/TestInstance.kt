6558382d292000bed6d63ea05e28541e2e17da47
Johannes Schaanning <johannes.schaanning@statnett.no>
2024-12-02 10:34:26 +0100

    chore: remove empty default constructor to make detekt happy

--- src/test/kotlin/no/elhub/devxp/TestInstance.kt
+++ src/test/kotlin/no/elhub/devxp/TestInstance.kt
@@ -1,43 +1,39 @@
 package no.elhub.devxp
 
+import org.apache.commons.io.FileUtils
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import java.io.File
 import java.nio.file.Files.createTempDirectory
 import java.nio.file.Path
-import org.apache.commons.io.FileUtils
 
-class TestInstance() {
+class TestInstance {
     private val projectDir: Path = createTempDirectory("test")
-    var settingsFile: File = File(projectDir.toString(), "settings.gradle.kts")
-    var buildFile: File =  File(projectDir.toString(), "build.gradle.kts")
-    var propertiesFile: File =  File(projectDir.toString(), "gradle.properties")
+    private var settingsFile: File = File(projectDir.toString(), "settings.gradle.kts")
+    var buildFile: File = File(projectDir.toString(), "build.gradle.kts")
+    var propertiesFile: File = File(projectDir.toString(), "gradle.properties")
 
     init {
-        settingsFile.appendText("""
-            rootProject.name = "test"
-        """)
-        propertiesFile.appendText("""
-            kotlin.code.style=official
-            version=0.0.0
-        """)
+        settingsFile.appendText(
+            """
+                rootProject.name = "test"
+            """
+        )
+        propertiesFile.appendText(
+            """
+                kotlin.code.style=official
+                version=0.0.0
+            """
+        )
     }
 
-    fun runTask(task: String): BuildResult {
-        return GradleRunner.create()
-                .withProjectDir(projectDir.toFile())
-                .withArguments(task, "--stacktrace")
-                .withPluginClasspath()
-                .build()
-    }
-
-    fun runTaskWithFailure(task: String): BuildResult {
-        return GradleRunner.create()
-                .withProjectDir(projectDir.toFile())
-                .withArguments(task, "--stacktrace")
-                .withPluginClasspath()
-                .buildAndFail()
-    }
+    fun runTask(task: String): BuildResult =
+        GradleRunner
+            .create()
+            .withProjectDir(projectDir.toFile())
+            .withArguments(task, "--stacktrace")
+            .withPluginClasspath()
+            .build()
 
     fun dispose() {
         FileUtils.deleteDirectory(projectDir.toFile())
