ad2e6bcbca3d2e1603b3bc577ba967c723caba47
Michael Akinde <michael.akinde@statnett.no>
2026-01-28 10:24:46 +0100

    Add coverage testing

--- src/test/kotlin/no/elhub/devxp/KotlinCoreTest.kt
+++ src/test/kotlin/no/elhub/devxp/KotlinCoreTest.kt
@@ -2,12 +2,15 @@ package no.elhub.devxp
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import org.gradle.testfixtures.ProjectBuilder
 import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
 import org.gradle.testing.jacoco.tasks.JacocoReport
 import org.gradle.testkit.runner.UnexpectedBuildFailure
+import kotlin.apply
+import kotlin.collections.set
 
 class KotlinCoreTest : FunSpec({
     val testInstance = TestInstance()
@@ -170,19 +173,34 @@ class KotlinCoreTest : FunSpec({
             result.output shouldContain ":dependencyCheckAnalyze"
             result.output shouldContain "BUILD SUCCESSFUL"
         }
+
+        test("should work with -PdependencyCHeck.suppressionFile") {
+            val result = testInstance.runTask("dependencyCheckAnalyze", "-PdependencyCheck.suppressionFile=suppressions.xml", "--dry-run")
+            result.output shouldContain ":dependencyCheckAnalyze"
+            result.output shouldContain "BUILD SUCCESSFUL"
+        }
+
+        test("should configure dependencyCheck suppression file via Project property") {
+            val projectWithSuppression = ProjectBuilder.builder().build()
+            projectWithSuppression.extensions.extraProperties["dependencyCheck.suppressionFile"] = "suppressions.xml"
+            projectWithSuppression.pluginManager.apply("no.elhub.devxp.kotlin-core")
+            val extension =
+                projectWithSuppression.extensions.getByName("dependencyCheck") as org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension
+            extension.suppressionFile.get() shouldBe "suppressions.xml"
+        }
     }
 
     context("When dokka is run with this plugin") {
 
         test("dokkaGfm task should run successfully") {
-            val result = testInstance.runTask("dokkaGfm", "--dry-run")
-            result.output shouldContain ":dokkaGfm"
+            val result = testInstance.runTask("dokkaGenerate", "--dry-run")
+            result.output shouldContain ":dokkaGenerate"
             result.output shouldContain "BUILD SUCCESSFUL"
         }
 
         test("dokkaHtml task should run successfully") {
-            val result = testInstance.runTask("dokkaHtml", "--dry-run")
-            result.output shouldContain ":dokkaHtml"
+            val result = testInstance.runTask("dokkaGenerateHtml", "--dry-run")
+            result.output shouldContain ":dokkaGenerateHtml"
             result.output shouldContain "BUILD SUCCESSFUL"
         }
     }
