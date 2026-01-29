28db8fefaf212c142492a371b94bcddccfdfe591
Michael Akinde <michael.akinde@statnett.no>
2026-01-29 13:14:32 +0100

    Add code coverage on kotlin applications
    ## 📝 Description
    
    Improve the code coverage of the shadowJar code.
    
    ## 🔗 Issue ID(s): [TDX-1051](https://elhub.atlassian.net/browse/TDX-1051)
    
    ## 📋 Checklist
    
    * ✅ Lint checks passed on local machine.
    * ✅ Unit tests passed on local machine.
    
    
    [TDX-1051]: https://elhub.atlassian.net/browse/TDX-1051?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/test/kotlin/no/elhub/devxp/KotlinApplicationTest.kt
+++ src/test/kotlin/no/elhub/devxp/KotlinApplicationTest.kt
@@ -1,5 +1,6 @@
 package no.elhub.devxp
 
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
@@ -26,7 +27,8 @@ class KotlinApplicationTest : FunSpec({
     context("When kotlin-application is built") {
 
         val project = ProjectBuilder.builder().build()
-        project.extensions.extraProperties.set("applicationMainClass", "no.elhub.test.TestMainKt")
+        val mainClass = "no.elhub.test.TestMainKt"
+        project.extensions.extraProperties.set("applicationMainClass", mainClass)
         project.pluginManager.apply("no.elhub.devxp.kotlin-application")
 
         val pluginsIncluded =
@@ -46,6 +48,23 @@ class KotlinApplicationTest : FunSpec({
         pluginsIncluded.forEach { plugin ->
             test("The project should include the $plugin plugin") {
                 project.plugins.hasPlugin(plugin) shouldBe true
+            }
+        }
+
+        test("should configure the shadowJar task correctly") {
+            val shadowTask = project.tasks.getByName("shadowJar") as ShadowJar
+            // Verify it produces a classifier-less jar (replacing the default jar)
+            shadowTask.archiveClassifier.get() shouldBe ""
+
+            // Verify Manifest attributes
+            val attributes = shadowTask.manifest.attributes
+            attributes["Main-Class"] shouldBe mainClass
+        }
+
+        test("should disable standard distribution tasks") {
+            // These tasks are redundant when using shadowJar for application distribution
+            listOf("jar", "distTar", "distZip", "startScripts").forEach { taskName ->
+                project.tasks.getByName(taskName).enabled shouldBe false
             }
         }
     }
@@ -84,5 +103,14 @@ class KotlinApplicationTest : FunSpec({
     }
     afterSpec {
         testInstance.dispose()
+    }
+
+    context("When assemble is run with this plugin") {
+
+        test("assemble task should depend on shadowJar") {
+            val result = testInstance.runTask("assemble", "--dry-run")
+            result.output shouldContain ":shadowJar"
+            result.output shouldContain "BUILD SUCCESSFUL"
+        }
     }
 })
